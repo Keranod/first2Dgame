c1e3a2f0df7bc3633ed7635fa858202d507a5e5e
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-23 15:03:38 +0100

    game done

--- Player.cs
+++ Player.cs
@@ -5,11 +5,31 @@ public partial class Player : Area2D
 {
 	[Export]
 	public int Speed { get; set; } = 400;
+
+	[Signal]
+	public delegate void HitEventHandler();
+
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+
 	public Vector2 ScreenSize;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
+		Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -47,6 +67,25 @@ public partial class Player : Area2D
 		else
 		{
 			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			// See the note below about the following boolean assignment.
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
 		}
 	}
 }
